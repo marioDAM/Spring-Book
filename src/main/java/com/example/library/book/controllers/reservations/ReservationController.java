161aa590293e0d828d6087ba3bd6246c92ab9254
MarioDam <marioturan.mv@gmail.com>
2022-03-04 14:02:56 +0100

    Añadiendo gestion de errores y creacion de paquetes para empezar con los test

--- src/main/java/com/example/library/book/controllers/reservations/ReservationController.java
+++ src/main/java/com/example/library/book/controllers/reservations/ReservationController.java
@@ -16,7 +16,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
+
 
 @RestController
 @RequestMapping(APIConfig.API_PATH + "/reservations")
