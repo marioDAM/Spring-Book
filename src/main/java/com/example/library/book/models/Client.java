eebe7b82c265cc80f415ab7eebbca1b1403bdd25
MarioDam <marioturan.mv@gmail.com>
2022-03-08 05:12:34 +0100

    añadiendo diagrama de clases y contenedor de docker

--- src/main/java/com/example/library/book/models/Client.java
+++ src/main/java/com/example/library/book/models/Client.java
@@ -9,8 +9,6 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.NotNull;
 import java.util.Collection;
 import java.util.List;
